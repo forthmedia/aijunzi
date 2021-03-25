6c16e222608599969b9de45611944c2df76a6d8e
Kurt Jonach <kurt@forth-media.com>
2021-03-25 16:06:27 -0700

    Implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { JunziLazyComponent } from './components/junzi-lazy/junzi-lazy.component';
+
+const routes: Routes = [
+  { path: 'lazy', component: JunziLazyComponent },
+  { path: '', redirectTo: '/lazy', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
