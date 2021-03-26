20f642d9ef44679b3bd17cad111680fcb023e592
Kurt Jonach <kurt@forth-media.com>
2021-03-26 11:43:38 -0700

    Adjust DevOps setting for deployment

--- src/app/components/junzi-lazy/junzi-lazy.component.ts
+++ src/app/components/junzi-lazy/junzi-lazy.component.ts
@@ -6,11 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./junzi-lazy.component.scss']
 })
 export class JunziLazyComponent {
-  image: string = '/assets/images/mpav-1.png';
+  image: string = './assets/images/mpav-1.png';
   deferred: string[] = [
-    '/assets/images/mpav-2.png',
-    '/assets/images/mpav-3.png',
-    '/assets/images/mpav-4.png',
-    '/assets/images/mpav-5.png'
+    './assets/images/mpav-2.png',
+    './assets/images/mpav-3.png',
+    './assets/images/mpav-4.png',
+    './assets/images/mpav-5.png'
   ];
 }
